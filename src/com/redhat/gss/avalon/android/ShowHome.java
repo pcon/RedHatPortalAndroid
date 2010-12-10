fbc5977c9a3793628f44c276ee1b1d8cec06a132
Ian Page Hands <iphands@gmail.com>
2010-12-10 11:15:14 -0500

    removed unused imports, and rmed target/

--- src/com/redhat/gss/avalon/android/ShowHome.java
+++ src/com/redhat/gss/avalon/android/ShowHome.java
@@ -3,10 +3,6 @@ package com.redhat.gss.avalon.android;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.redhat.gss.strata.model.Case;
-import com.redhat.gss.strata.model.Cases;
-import com.redhat.gss.strata.resource.CasesResource;
-
 
 public class ShowHome extends Activity {
     /** Called when the activity is first created. */
