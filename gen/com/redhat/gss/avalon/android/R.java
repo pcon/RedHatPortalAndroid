7be9fd23e4820c2437892892b2fbd6e18a6ba597
Ian Page Hands <iphands@gmail.com>
2010-12-10 18:15:16 -0500

    removing R.java it should not be checked in

--- gen/com/redhat/gss/avalon/android/R.java
+++ gen/com/redhat/gss/avalon/android/R.java
@@ -8,16 +8,46 @@
 package com.redhat.gss.avalon.android;
 
 public final class R {
+    public static final class array {
+        public static final int main_menu_array=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int about=0x7f070008;
+        public static final int commentList=0x7f070003;
+        public static final int mainList=0x7f070004;
+        public static final int new_case=0x7f070005;
+        public static final int setup=0x7f070007;
+        public static final int sort=0x7f070006;
+        public static final int status=0x7f070002;
+        public static final int title=0x7f070001;
+        public static final int webview=0x7f070000;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int about=0x7f030000;
+        public static final int case_view=0x7f030001;
+        public static final int list_item=0x7f030002;
+        public static final int main=0x7f030003;
+    }
+    public static final class menu {
+        public static final int menu=0x7f060000;
     }
     public static final class string {
+        public static final int about=0x7f040004;
+        public static final int about_message=0x7f040008;
+        public static final int about_title=0x7f040007;
         public static final int app_name=0x7f040001;
+        public static final int caseView_title=0x7f040009;
+        public static final int caseView_top_status=0x7f04000b;
+        public static final int caseView_top_title=0x7f04000a;
         public static final int hello=0x7f040000;
+        public static final int new_case=0x7f040002;
+        public static final int okay=0x7f040006;
+        public static final int setup=0x7f040003;
+        public static final int sort=0x7f040005;
     }
 }
