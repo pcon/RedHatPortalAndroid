6ccf2581025bed203bc50e60651ecf2e422f4d05
Patrick Connelly <pconnell@redhat.com>
2010-12-10 10:46:30 -0500

    Adding imports of Case, Cases and CasesResource

--- src/com/redhat/gss/avalon/android/ShowHome.java
+++ src/com/redhat/gss/avalon/android/ShowHome.java
@@ -3,6 +3,11 @@ package com.redhat.gss.avalon.android;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.redhat.gss.strata.model.Case;
+import com.redhat.gss.strata.model.Cases;
+import com.redhat.gss.strata.resource.CasesResource;
+
+
 public class ShowHome extends Activity {
     /** Called when the activity is first created. */
     @Override
