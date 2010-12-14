0d3d9871259966c5a247478d825607c81a5a752e
Patrick Connelly <patrick@deadlypengin.com>
2010-12-14 11:48:28 -0500

    Making CaseController pull from CaseParser to be able to add Caching

--- src/com/redhat/gss/avalon/android/CaseController.java
+++ src/com/redhat/gss/avalon/android/CaseController.java
@@ -39,33 +39,8 @@ public class CaseController {
 	}
 
 	public List<Case> getAllCases() {
-		Case kase = new Case();
-
-		kase.setCaseNumber("1234");
-		kase.setSummary("[1] This is a sample test kase");
-		kase.setDescription("[1] This is a sample description");
-		kase.setSeverity("1 (Urgent)");
-
-		Case kase2 = new Case();
-
-		kase2.setCaseNumber("1235");
-		kase2.setSummary("[2] This is a sample test kase");
-		kase2.setDescription("[2] This is a sample description");
-		kase2.setSeverity("2 (High)");
-
-		Case kase3 = new Case();
-
-		kase3.setCaseNumber("1236");
-		kase3.setSummary("[3] This is a sample test kase");
-		kase3.setDescription("[3] This is a sample description");
-		kase3.setSeverity("3 (Medium)");
-
-		List<Case> kases = new ArrayList<Case>();
-		kases.add(kase);
-		kases.add(kase2);
-		kases.add(kase3);
-
-		return kases;
+		CaseParser cp = new CaseParser();
+		return cp.getAllCases();
 	}
 
 	public List<Comment> getComments(String caseNumber) {
